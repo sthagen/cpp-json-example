ea0210af2e9ed0142559974d86a5924676fd2233
Stefan Hagen <stefan@hagen.link>
2020-12-30 23:08:30 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
+
+int main() {
+  auto j = R"(
+    {
+      "happy": true,
+      "pi": 3.14156
+    }
+  )"_json;
+  std::string s = j.dump();
+
+  std::cout << "compact:  " << s << "\n";
+  std::cout << "indented:\n" 
+            << j.dump(4) << "\n";
+  
+}
